64d5dd3ed7ed242dd7d9b6549f12b7da4e8d5782
Ricky-Kwon <ricky@wiznet.co.kr>
2018-08-06 08:42:11 +0900

    Add IoT mood lamp

---
+++ IoT_Mood_Lamp/_projects/Exercise2/Exercise2.ino
@@ -0,0 +1,59 @@
+#include "DHT.h"
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define DHTPIN 2     
+#define PIN            6
+#define NUMPIXELS      2
+#define DHTTYPE DHT11   
+
+DHT dht(DHTPIN, DHTTYPE);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+void setup() {
+#if defined (__AVR_ATtiny85__)
+  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+#endif
+  Serial.begin(9600);
+  Serial.println("DHTxx test!");
+
+  dht.begin();
+  pixels.begin(); // This initializes the NeoPixel library.
+}
+
+void loop() {
+
+  delay(2000);
+  
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float f = dht.readTemperature(true);
+  
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  float hif = dht.computeHeatIndex(f, h);
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hic);
+  Serial.print(" *C ");
+  Serial.print(hif);
+  Serial.println(" *F");
+
+  pixels.setPixelColor(0, pixels.Color(0, 0, map(h, 20, 90, 0, 255))); // bright blue color.
+  pixels.setPixelColor(1, pixels.Color(map(t, 0, 50, 0, 255), 0, 0)); // bright red color.
+  
+  pixels.show(); // This sends the updated pixel color to the hardware.
+}
---
+++ IoT_Mood_Lamp/_projects/IoT_mood_lamp/IoT_mood_lamp.ino
@@ -0,0 +1,70 @@
+#define BLYNK_PRINT  Serial
+#define SERIAL_WIFI  Serial3
+#define NEOPIXEL_PIN 6
+#define NEOPIXEL_LED_MAX 2
+/* For DHT11 Sensor */
+#define DHTPIN            2     
+#define DHTTYPE           DHT11  
+
+#include "BlynkSimpleWizFi310.h"
+#include <Adafruit_NeoPixel.h> 
+#include "DHT.h"
+
+DHT dht(DHTPIN, DHTTYPE);
+BlynkTimer timer;
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NEOPIXEL_LED_MAX, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
+char auth[] = "";
+char ssid[] = "";
+char pass[] = "";
+
+BLYNK_WRITE(V2)
+{
+  int r = param[0].asInt();
+  int g = param[1].asInt();
+  int b = param[2].asInt();
+
+  for (int i = 0; i < strip.numPixels(); i++)
+  {
+    strip.setPixelColor(i, r, g, b);
+  }
+  strip.show(); 
+}
+void sendSensor()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V0, t);
+  Blynk.virtualWrite(V1, h);
+}
+
+void setup()
+{
+  strip.begin();                                      
+  strip.show();
+  // Debug console
+  Serial.begin(9600);
+  SERIAL_WIFI.begin(115200);
+
+  WiFi.init(&SERIAL_WIFI);
+  
+  Blynk.begin(auth, ssid, pass);
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
+  dht.begin();
+  // Setup a function to be called every second
+  timer.setInterval(1000L, sendSensor);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+}
+
